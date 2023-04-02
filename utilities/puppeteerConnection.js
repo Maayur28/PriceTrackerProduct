31852b74755051ea9723909dd82883fc87b4b8f1
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-04-02 14:11:52 +0530

    added error message

--- utilities/puppeteerConnection.js
+++ utilities/puppeteerConnection.js
@@ -5,7 +5,10 @@ const getUserAgent = require("../utilities/useragents");
 let connection = {};
 connection.initialisePuppeteer = async (URL) => {
   try {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--headless", "--no-sandbox"],
+    });
     const page = await browser.newPage();
     let userAgent = getUserAgent();
     await page.setUserAgent(userAgent);
@@ -16,6 +19,7 @@ connection.initialisePuppeteer = async (URL) => {
     await browser.close();
     return $;
   } catch (error) {
+    console.log(error.message);
     let err = new Error();
     err.message = "Could not establish connection with server.";
     err.status = 403;
