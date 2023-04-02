63524dc13a3060b97379701b30451ce64b6c8dc8
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-04-02 18:55:16 +0530

    added puppeteer fix

--- utilities/puppeteerConnection.js
+++ utilities/puppeteerConnection.js
@@ -1,15 +1,23 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
+const chromium = require("chrome-aws-lambda");
 const getUserAgent = require("../utilities/useragents");
 
 let connection = {};
 connection.initialisePuppeteer = async (URL) => {
   try {
-    const browser = await puppeteer.launch({
-      ignoreDefaultArgs: ["--disable-extensions"],
-      headless: false,
-      args: ["--no-sandbox", "--disabled-setupid-sandbox"],
-      'ignoreHTTPSErrors': true
+    // const browser = await puppeteer.launch({
+    //   ignoreDefaultArgs: ["--disable-extensions"],
+    //   headless: false,
+    //   args: ["--no-sandbox", "--disabled-setupid-sandbox"],
+    //   'ignoreHTTPSErrors': true
+    // });
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
     });
     const page = await browser.newPage();
     let userAgent = getUserAgent();
