80200242392ddc6189ea5028dbf4a58e0bd16327
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-04-26 09:42:53 +0530

    added fix

--- utilities/telegram.js
+++ utilities/telegram.js
@@ -10,9 +10,13 @@ telegram.scrapped = async (title, price, URL, discountPrice) => {
   try {
     let droppedPrice = discountPrice - price;
     let message = `<strong>Price Dropped by ${droppedPrice}</strong>\r\n<pre>${title}</pre>\r\n\n<a href="${URL}">View Product</a>`;
-    await bot.sendMessage(process.env.TELEGRAM_PRICETRACKER_GROUP_CHAT_ID, message, {
-      parse_mode: "HTML",
-    });
+    await bot.sendMessage(
+      process.env.TELEGRAM_PRICETRACKER_GROUP_CHAT_ID,
+      message,
+      {
+        parse_mode: "HTML",
+      }
+    );
   } catch (error) {
     console.log(error.message);
   }
@@ -20,10 +24,12 @@ telegram.scrapped = async (title, price, URL, discountPrice) => {
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
-  console.log(msg);
+  let message = `Welcome to PriceTracker <strong>${data.first_name}</strong>\r\n\n<a href="https://www.trackprice.co.in/">View Website</a>`;
   if (msg.new_chat_members) {
     msg.new_chat_members.forEach((data) => {
-      bot.sendMessage(chatId, `Welcome to PriceTracker ${data.first_name}`);
+      bot.sendMessage(chatId, message, {
+        parse_mode: "HTML",
+      });
     });
   }
 });
