3d4cc1802eff5fd6f253b7776764a65782104cb4
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-05-16 08:04:46 +0530

    added fix

--- utilities/telegram.js
+++ utilities/telegram.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const token = process.env.TELEGRAM_PRICETRACKER_BOT_TOKEN;
 
-const bot = new TelegramBot(token, { polling: false });
+const bot = new TelegramBot(token, { polling: true });
 
 let telegram = {};
 let msgg = null,
